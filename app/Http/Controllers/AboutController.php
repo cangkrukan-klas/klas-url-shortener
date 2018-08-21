94a23e062cb9e1e50bd8ea8632c6c85fdc3c8c52
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2018-08-21 12:17:56 +0700

    Menambahkan tampilan about pada halaman admin

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('about', ['page_title' => 'About']);
+    }
+}
