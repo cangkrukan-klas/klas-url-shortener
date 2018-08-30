94afab03bf1b9f9cbb12d3eb7b23e9d9b7fdb156
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2018-08-30 14:20:53 +0700

    Mengganti waktu menunggu redirecting (5s -> 3s)

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -25,7 +25,7 @@
 @section('jsscript')
     <script>
         // Set the date we're counting down to
-        var countDown = 6;
+        var countDown = 4;
 
         // Update the count down every 1 second
         var x = setInterval(function() {
