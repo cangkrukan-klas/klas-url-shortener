1ecfad1e6d04cb76145f667b972de1b1128efe2b
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2019-01-12 11:35:18 +0700

    fix CSS bug #2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,6 +22,42 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .row {
+            margin-bottom: 3%;
+        }
+
+        html {
+            position: relative;
+            min-height: 100%;
+        }
+
+        body {
+            margin-bottom: 60px; /* Margin bottom by footer height */
+            background-color: white;
+            font-family: Nunito, Arial, sans-serif;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 60px; /* Set the fixed height of the footer here */
+            line-height: 60px; /* Vertically center the text there */
+            background-color: #f5f5f5;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            font-family: Nunito, Arial, sans-serif;
+            line-height: 1.1;
+            font-weight: bolder;
+            color: inherit;
+        }
+
+        .py-4 {
+            padding-bottom: 0 !important;
+        }
+    </style>
 </head>
 <body>
 <div id="app">
