8a8d8216ff976228dd45b88e009443b274ba44ca
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2019-02-08 19:20:36 +0700

    Tampilan lebih responsif untuk mobile

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,17 +35,38 @@
     </style>
 </head>
 <body id="app">
-<header></header>
+<header>
+    <div class="navbar-fixed hide-on-med-and-up">
+        <nav class="teal z-depth-0">
+            <div class="nav-wrapper">
+                <a href="#" data-target='dropdown1' class='dropdown-trigger'><i class="material-icons">menu</i></a>
+                <a href="https://klas.or.id" class="brand-logo"><img height="56px" width="auto" alt="KLAS LOGO"
+                                                                                  src="{{ asset('img/logo.png') }}"></a>
+            </div>
+        </nav>
+    </div>
+
+    <ul id='dropdown1' class='dropdown-content'>
+        <li><a class="valign-wrapper" style="display: flex;" href="https://github.com/cangkrukan-klas/klas-url-shortener">
+                <img alt="Github Repository" style="padding: 2px;" width="30px" height="auto" src="{{ asset("img/github/GitHub-Mark-32px.png") }}">
+                GitHub Repository
+            </a>
+        </li>
+        <li><a href="https://klas.or.id">Tentang kami</a> </li>
+        <li class="divider" tabindex="-1"></li>
+        <li><a href="#!">Tautan pendek dibuat : {{ (\App\DataStatistik::query()->where('nama', 'shortlinkgenerate')->first()->nilai) + (\App\DataStatistik::query()->where('nama', 'shortlinkcustom')->first()->nilai) }}</a></li>
+    </ul>
+</header>
 <main><div class="container valign-wrapper">
         <div class="row center-align" style="padding-top: 6%;">
             <div class="col s12 m12 l12">
-                <div class="row center-align">
+                <div class="row center-align hide-on-small-only">
                     <a href="https://klas.or.id"><img class="responsive-img" alt="KLAS LOGO"
                                                       src="{{ asset('img/logo.png') }}"></a>
                 </div>
-                <div class="row center-align">
+                <div class="row center-align flow-text">
                     <h4>PEMENDEK TAUTAN SEDERHANA DAN CEPAT</h4>
-                    <p style="font-size: 16px;">oleh Kelompok Linux Arek Suroboyo</p>
+                    <p class="hide-on-small-only">oleh Kelompok Linux Arek Suroboyo</p>
                 </div>
                 <div class="col s8 m8 l8 offset-s2 offset-m2 offset-l2 left-align">
                     <main>
@@ -57,16 +78,21 @@
     </div></main>
 <!-- Footer -->
 <footer class="footer-copyright">
-    <div class="row valign-wrapper center-align">
+    <div class="row valign-wrapper center-align hide-on-small-only">
         <div class="col s4 m4 l4">2018</div>
         <div class="col s4 m4 l4">Tautan pendek dibuat
             : {{ (\App\DataStatistik::query()->where('nama', 'shortlinkgenerate')->first()->nilai) + (\App\DataStatistik::query()->where('nama', 'shortlinkcustom')->first()->nilai) }}</div>
         <div class="col s4 m4 l4"><span class="text-muted"><a
-                        href="https://github.com/fadhilyori/klas-url-shortener"><img alt="Github Repository" src="{{ asset("img/github/GitHub-Mark-32px.png") }}"></a></span></div>
+                        href="https://github.com/cangkrukan-klas/klas-url-shortener"><img alt="Github Repository" src="{{ asset("img/github/GitHub-Mark-32px.png") }}"></a></span></div>
     </div>
 </footer>
 <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
-<script>  M.AutoInit();</script>
+<script>
+    M.AutoInit();
+    $(document).ready(function(){
+        $('.sidenav').sidenav();
+    });
+</script>
 @yield('jsscript')
 </body>
 </html>
