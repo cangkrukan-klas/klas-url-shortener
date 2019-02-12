ad48a6a556b8a3581a1f5377acbe1fc7c980ede0
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2019-02-12 19:49:19 +0700

    Menambahkan halaman untuk admin dan banyak perbaikan bug #2

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Icons -->
+    <link rel="icon" href="{{ asset('img/logo-32x32.png') }}" sizes="32x32">
+    <title>URL Shortener by KLAS</title>
+
+    <!-- Styles -->
+    <link type="text/css" href="{{ asset('css/normalize.css') }}" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.css') }}" media="screen,projection"/>
+    <link type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <style>
+        body {
+            display: flex;
+            min-height: 100vh;
+            flex-direction: column;
+        }
+
+        main {
+            flex: 1 0 auto;
+        }
+
+        .input-field input[type=date]:focus + label,
+        .input-field input[type=text]:focus + label,
+        .input-field input[type=email]:focus + label,
+        .input-field input[type=password]:focus + label {
+            color: #e91e63;
+        }
+
+        .input-field input[type=date]:focus,
+        .input-field input[type=text]:focus,
+        .input-field input[type=email]:focus,
+        .input-field input[type=password]:focus {
+            border-bottom: 2px solid #e91e63;
+            box-shadow: none;
+        }
+    </style>
+</head>
+<body id="app">
+<header>
+</header>
+<main>
+    <div class="container valign-wrapper hide-on-small-and-down" style="padding: 6%;">
+        <div class="z-depth-5 grey lighten-4 row" id="inibro" style="display: inline-block; padding: 32px 64px 0px 64px; border: 1px solid #EEE;">
+            <div class="center">
+                <div style="font-size: 32px;">{{ __('Register') }}</div>
+            </div>
+
+            <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                @csrf
+
+                <div class='row'>
+                    <div class='col s12'>
+                    </div>
+                </div>
+
+                <div class="row input-field">
+                    <input id="name" type="email" class="validate" name="name" value="{{ old('name') }}" required>
+                    <label for="name">{{ __('Name') }}</label>
+                </div>
+
+                <div class="row input-field">
+                    <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required>
+                    <label for="email">{{ __('E-Mail Address') }}</label>
+                </div>
+
+                <div class="row input-field">
+                    <input id="password" type="password" class="validate" name="password" value="{{ old('password') }}" required>
+                    <label for="password">{{ __('Password') }}</label>
+                </div>
+
+                <div class="row input-field">
+                    <input id="password-confirm" type="password" class="validate" name="password_confirmation" value="{{ old('password_confirmation') }}" required>
+                    <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                </div>
+
+                <div class="row">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">{{ __('Register') }}</button>
+                </div>
+
+                <div class="row">
+                    <span>Sudah punya akun ? <a href="{{ route('login') }}">Masuk disini</a></span>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="hide-on-med-and-up">
+        <nav class="navbar-fixed-top teal z-depth-0">
+            <div class="nav-wrapper">
+                <div class="nav-title center-align" style="display: block; padding: 0;">{{ __('Register') }}</div>
+            </div>
+        </nav>
+        <div class="section"></div>
+        <div class="container">
+            <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                @csrf
+
+                <div class='row'>
+                    <div class='col s12'>
+                    </div>
+                </div>
+
+                <div class="row input-field">
+                    <input id="name" type="email" class="validate" name="name" value="{{ old('name') }}" required>
+                    <label for="name">{{ __('Name') }}</label>
+                </div>
+
+                <div class="row input-field">
+                    <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}" required>
+                    <label for="email">{{ __('E-Mail Address') }}</label>
+                </div>
+
+                <div class="row input-field">
+                    <input id="password" type="password" class="validate" name="password" value="{{ old('password') }}" required>
+                    <label for="password">{{ __('Password') }}</label>
+                </div>
+
+                <div class="row input-field">
+                    <input id="password-confirm" type="password" class="validate" name="password_confirmation" value="{{ old('password_confirmation') }}" required>
+                    <label for="password-confirm">{{ __('Confirm Password') }}</label>
+                </div>
+
+                <div class="row">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">{{ __('Register') }}</button>
+                </div>
+
+                <div class="row">
+                    <span>Sudah punya akun ? <a href="{{ route('login') }}">Masuk disini</a></span>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+<!-- Footer -->
+<footer class="footer-copyright">
+
+</footer>
+<script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
+<script>
+    M.AutoInit();
+</script>
+<script>
+    // Set the date we're counting down to
+    let countDown = 9;
+    let data = ["z-depth-5", "z-depth-4", "z-depth-3", "z-depth-2", "z-depth-1", "z-depth-0"];
+    let i = 0;
+    // Update the count down every 1 second
+    let x = setInterval(function () {
+        countDown--;
+        // Output the result in an element with id="demo"
+        if (countDown<5) {
+            if ( document.getElementById("inibro").classList.contains(data[i]) ) {
+                document.getElementById("inibro").classList.replace(data[i], data[i-1]);
+            }
+            i--;
+        } else {
+            if ( document.getElementById("inibro").classList.contains(data[i]) ) {
+                document.getElementById("inibro").classList.replace(data[i], data[i+1]);
+            }
+            i++;
+        }
+        // If the count down is over, write some text
+        if (countDown <= 0) {
+            clearInterval(x);
+            if ( document.getElementById("inibro").classList.contains(data[i]) ) {
+                document.getElementById("inibro").classList.replace(data[i], "z-depth-5");
+            }
+        }
+    }, 100);
+</script>
+</body>
+</html>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Icons -->
+    <link rel="icon" href="{{ asset('img/logo-32x32.png') }}" sizes="32x32">
+    <title>URL Shortener by KLAS</title>
+
+    <!-- Styles -->
+    <link type="text/css" href="{{ asset('css/normalize.css') }}" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.css') }}" media="screen,projection"/>
+    <link type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <style>
+        body {
+            display: flex;
+            min-height: 100vh;
+            flex-direction: column;
+        }
+        main {
+            flex: 1 0 auto;
+        }
+
+        header, main, footer {
+            padding-left: 300px;
+        }
+
+        @media only screen and (max-width : 992px) {
+            header, main, footer {
+                padding-left: 0;
+            }
+        }
+    </style>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
+    <script>
+        M.AutoInit();
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+        });
+    </script>
+    @yield('jsscript')
+</head>
+<body id="app">
+<header>
+    <nav class="navbar-fixed teal z-depth-0">
+        <div class="nav-wrapper">
+            <a href="#" data-target='slide-out' class='sidenav-trigger show-on-small show-on-medium-and-up left'><i class="material-icons">menu</i></a>
+            <span class="white-text center" style="margin-top: 0; margin-bottom: 0; width: 100%;">Halaman Admin s.klas.or.id</span>
+            <span class="right"><a href="{{ url('logout') }}" class="btn waves-effect waves-light red">Logout</a></span>
+        </div>
+    </nav>
+
+    <ul id="slide-out" class="sidenav sidenav-fixed">
+        <li><div class="user-view">
+                <div class="background teal"></div>
+                <img class="circle" src="{{ asset("img/logo-192x192.png") }}">
+                <span class="white-text name">Admin</span>
+                <span class="white-text email">admin@example.com</span>
+            </div></li>
+        <li><a href="{{ route('admin.dashboard') }}" class="waves-effect"><i class="material-icons">dashboard</i>Dasboard</a></li>
+        <li><a href="{{ route('admin.shorturl') }}" class="waves-effect"><i class="material-icons">table</i>Tautan Pendek</a></li>
+        <li><a href="{{ route('admin.customurl') }}" class="waves-effect"><i class="material-icons">table</i>Tautan Custom</a></li>
+        <li><div class="divider"></div></li>
+        <li><a class="subheader">Opsi</a></li>
+        <li><a href="{{ url('logout') }}" class="waves-effect">Logout</a></li>
+    </ul>
+</header>
+<main>
+    @yield('content')
+</main>
+<!-- Footer -->
+<footer></footer>
+</body>
+</html>
